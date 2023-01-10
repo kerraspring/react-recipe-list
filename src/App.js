567ed853ae94dbf9ab722a12ab9c90f452a6cfec
Kerra Springgay <kerraspring@gmail.com>
2023-01-10 14:10:32 -0600

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import RecipeList from "./components/RecipeList";
+import './css/app.css'
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RecipeList recipes={sampleRecipes}/>
+  )
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Chicken',
+    servings: 2,
+    cookTime: '2:00',
+    instructions: '1. salt the meat\n2. cook the meat\n3. eat the meat',
+    ingredients: [
+      {
+        id: 1,
+        name: 'chicken',
+        amount: '1 Lbs'
+      },
+      {
+        id: 2,
+        name: 'salt',
+        amount: '1 Tbsp'
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Beef',
+    servings: 3,
+    cookTime: '2:30',
+    instructions: '1. salt the meat\n2. cook the meat\n3. eat the meat',
+    ingredients: [
+      {
+        id: 1,
+        name: 'beef',
+        amount: '1 Lbs'
+      },
+      {
+        id: 2,
+        name: 'salt',
+        amount: '1 Tbsp'
+      }
+    ]
+  },
+]
 
 export default App;
